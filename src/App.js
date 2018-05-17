4b9b5eef1d1764e4554ab46864f75380be5126ce
Drew Dowling <drewdowling37@gmail.com>
2018-05-16 22:30:44 -0700

    Added react-bootstrap modal
    Also removed the logo and other create-react-app defaults

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
+import TitleModal from './TitleModal.jsx'
+
+const Header = (props) => {
+  return <div className="Header"><h1>{props.title}</h1></div>;
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Who Would Win?</h1>
+        <Header title="Who Would Win?"/>
+        <TitleModal/>
       </div>
     );
   }
